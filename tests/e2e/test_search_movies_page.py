f9b3b8c25adbffd99fd25737836f1a7af4d5bc3a
cgarciaflor <cristiangarciaflor@gmail.com>
2023-03-16 13:45:18 -0400

    feature 3 html/test

--- tests/e2e/test_search_movies_page.py
+++ tests/e2e/test_search_movies_page.py
@@ -3,13 +3,12 @@ from flask.testing import FlaskClient
 from src.repositories.movie_repository import _movie_repo
 
 def test_search_movies_page(test_app: FlaskClient):
-    response = test_app.get("/movies/search")
 
     _movie_repo.create_movie("Star Wars Episode Three", "George Lucas", 5)
 
     response = test_app.get("/movies/search?title=Star+Wars+Episode+3")
 
-
-    assert response.status_code == 302
-    assert _movie_repo.get_movie_by_title('Star Wars') !=  None
+    
+    assert response.status_code == 200
+    assert _movie_repo.get_movie_by_title('Star Wars Episode Three') !=  None
 
--- tests/unit/test_get_movie_by_title.py
+++ tests/unit/test_get_movie_by_title.py
@@ -1,1 +1,14 @@
 # TODO: Feature 3
+from src.repositories.movie_repository import get_movie_repository
+from src.models.movie import Movie
+
+def get_movie_name():
+    test_movie = get_movie_repository()
+    test_movie.create_movie("Star Wars Episode 3", "George Lucas", 5)
+    get_title = test_movie.get_movie_by_title("Star Wars Episode 3")
+    
+    assert get_title.title == "Star Wars Episode 3"
+    assert get_title.director == "George Lucas"
+    assert get_title.rating == 5
+    
+
