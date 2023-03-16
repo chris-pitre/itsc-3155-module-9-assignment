bc849020ba0db77c39779cee91c5751db2f0f3d5
cgarciaflor <cristiangarciaflor@gmail.com>
2023-03-16 14:02:33 -0400

    unit test

--- tests/unit/test_get_movie_by_title.py
+++ tests/unit/test_get_movie_by_title.py
@@ -2,7 +2,7 @@
 from src.repositories.movie_repository import get_movie_repository
 from src.models.movie import Movie
 
-def get_movie_name():
+def test_get_movie_by_title():
     test_movie = get_movie_repository()
     test_movie.create_movie("Star Wars Episode 3", "George Lucas", 5)
     get_title = test_movie.get_movie_by_title("Star Wars Episode 3")
